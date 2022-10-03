8bba1b07836e785d641106acb0f4ec491074f337
meemory <mehmet@hotmail.com>
2022-10-03 09:43:00 +0200

    Grid System eingebaut

--- website/all/listall.js
+++ website/all/listall.js
@@ -1,37 +1,32 @@
-
-
 async function listAll() {
-    const xmlhttp = new XMLHttpRequest();
-    const url = "https://testapi.robli.at/machine/all";
-    xmlhttp.open("GET", url, true);
-    xmlhttp.send();
-    xmlhttp.onload = (res) => {
-  
-        console.log(res['target']['response']);
-        message = res['target']['response'];
-        const arr = message.split(',')
-        let i=0, j=0, a=0;
-        const len=arr.length;
-        arr.slice(0,1);
-        while(j<=len){
-        do{
-          i++;
-          a++
-        }while(i<= 2);
-        arr.splice(a, 1);
-        i=1;
-        j++;
-        }
-        const newlen=arr.length;
-        let l=0,t=0;
-        var node, testnode;
-        while(l<=newlen){
-          node=document.createElement("p");
-          testnode=document.createTextNode(arr[l]);
-          node.appendChild(testnode);
-          document.getElementById("enter").appendChild(node);
-          l++;
-        }
+  // while(l<=newlen){
+  //   node=document.createElement("p");
+  //   testnode=document.createTextNode(arr[l]);
+  //   node.appendChild(testnode);
+  //   document.getElementById("enter").appendChild(node);
+  // //   l++;}
+  //   }
+  let response = await fetch("https://testapi.robli.at/machine/all");
+  let data = await response.json();
 
-    }
+  let arr;
+  let i = 0;
+  while (i < data.length) {
+    arr = JSON.stringify(data[i]).split(",");
+    arr.shift();
+    console.log(arr);
+    
+    
+    div = document.createElement("div");
+    div.className= "col-md";
+    col = document.createElement("div");
+    col.className= "p-3 border bg-light";
+
+    text = document.createTextNode(arr.join(" "));
+    col.appendChild(text);
+    div.appendChild(col);
+    document.getElementById("check").appendChild(div);
+
+    i++;
+  }
 }
