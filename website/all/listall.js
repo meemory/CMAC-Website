f7c8c7a08551b9ed9c18a9b0b850c19ef7f845ac
meemory <mehmet@hotmail.com>
2022-10-05 11:25:06 +0200

    Implemented to listAll Method to allmachines.html

--- website/all/listall.js
+++ website/all/listall.js
@@ -1,32 +1,85 @@
+
 async function listAll() {
-  // while(l<=newlen){
-  //   node=document.createElement("p");
-  //   testnode=document.createTextNode(arr[l]);
-  //   node.appendChild(testnode);
-  //   document.getElementById("enter").appendChild(node);
-  // //   l++;}
-  //   }
   let response = await fetch("https://testapi.robli.at/machine/all");
   let data = await response.json();
 
-  let arr;
-  let i = 0;
-  while (i < data.length) {
-    arr = JSON.stringify(data[i]).split(",");
-    arr.shift();
-    console.log(arr);
-    
+  for(let i=0; i<data.length; i++) {
+    console.log(data[0].name);
+
     
     div = document.createElement("div");
-    div.className= "col-md";
+    div.className=  "bg-dark   p-2 g-3";
     col = document.createElement("div");
-    col.className= "p-3 border bg-light";
+    col.className= "card";
+    card = document.createElement("div");
+    card.className="card-body";   
+    cardtitle = document.createElement("h5");
+    cardtitle.className="card-title";
+    cardbody = document.createElement("span");
+    cardbody.className="card-text";
 
-    text = document.createTextNode(arr.join(" "));
-    col.appendChild(text);
+    header = document.createTextNode("Maschine " + (i+1));
+    cardbody.appendChild(document.createTextNode("Name: "+ data[i].name) );
+    cardbody.appendChild(document.createElement('br'));
+    cardbody.appendChild(document.createTextNode("Machinen ID: " + data[i].idusr));
+
+
+    cardtitle.appendChild(header);
+    card.appendChild(cardtitle);
+    card.appendChild(cardbody);
+    col.appendChild(card);
     div.appendChild(col);
     document.getElementById("check").appendChild(div);
 
-    i++;
   }
 }
+
+async function listAllPersons() {
+  let response = await fetch("https://testapi.robli.at/user/all");
+  let data = await response.json();
+
+  let response1 = await fetch("https://testapi.robli.at/machine/all");
+  let data1 = await response.json();
+
+  for(let i=0; i<data.length; i++) {
+    console.log(data[0].name);
+
+    
+    div = document.createElement("div");
+    div.className=  "bg-dark   p-2 g-3";
+    col = document.createElement("div");
+    col.className= "card";
+    card = document.createElement("div");
+    card.className="card-body";   
+    cardtitle = document.createElement("h5");
+    cardtitle.className="card-title";
+    cardbody = document.createElement("p");
+    cardbody.className="card-text";
+    cardbody1 = document.createElement("p");
+    cardbody1.className="card-text";
+
+
+    header = document.createTextNode("Person " + (i+1));
+    cardbody.appendChild(document.createTextNode("name: "+ data[i].name) );
+    cardbody.appendChild(document.createElement('br'));
+    cardbody.appendChild(document.createTextNode("surname: " + data[i].surname));
+    cardbody.appendChild(document.createElement('br'));
+    cardbody.appendChild(document.createTextNode("email: " + data[i].email));
+    cardbody.appendChild(document.createElement('br'));
+
+
+    cardbody1.appendChild(tesxt2);
+    cardbody.appendChild(text);
+    cardtitle.appendChild(header);
+    card.appendChild(cardtitle);
+    card.appendChild(cardbody);
+    card.appendChild(cardbody1);
+    col.appendChild(card);
+    div.appendChild(col);
+    document.getElementById("check").appendChild(div);
+
+  }
+}
+
+
+
